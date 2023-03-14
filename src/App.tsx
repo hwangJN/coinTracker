66601fbdd72bf9a0bf86b89bd5840024412c87e2
Hwang Jina <wlsk401@gmail.com>
2023-03-14 17:05:48 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ReactQueryDevtools } from "react-query/devtools";
 
 //style-components의 전역 스타일
 const GlobalStyle = createGlobalStyle`
-//@import url(''); ~!~!
+//@import url('');
 
   html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
